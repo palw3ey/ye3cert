a32d341534cd2ff5c8a496b120d87e758613c4a6
palw3ey <113945369+palw3ey@users.noreply.github.com>
2024-10-26 19:21:57 -0300

    Update bypass_container_env.sh

--- bypass_container_env.sh
+++ bypass_container_env.sh
@@ -9,7 +9,6 @@
 # export Y_IP_CHECK_EXTERNAL=yes
 # export Y_URL_IP_CHECK=http://whatismyip.akamai.com
 # export Y_URL_IP_CHECK_TIMEOUT=5
-# export Y_DEBUG=no
 
 ## http
 # export Y_HTTP=yes
@@ -27,17 +26,28 @@
 # export Y_OCSP_PORT=8080
 
 ## default certificate
+# export Y_KEY_SIZE=2048
 # export Y_DAYS=3650
-# export Y_DNS=
-# export Y_CN=
-# export Y_ORGANIZATION_NAME=Test
-# export Y_EMAIL_ADDRESS=webmaster@test.lan
-# export Y_COUNTRY_NAME=FR
-# export Y_STATE_OR_PROVINCE_NAME=Ile-de-France
-# export Y_LOCALITY_NAME=Paris
-# export Y_ORGANIZATIONAL_UNIT_NAME=Web
+# export Y_DAYS_CLIENT=365
 # export Y_KEY_USAGE="nonRepudiation, digitalSignature, keyEncipherment"
 # export Y_EXTENDED_KEY_USAGE="serverAuth, clientAuth"
+
+## Server
 # export Y_CA_PASS=ca
-# export Y_KEY_SIZE=2048
-# export Y_CREATE_TEST_CLIENT=no
+# export Y_DNS=
+# export Y_CN=
+# export Y_COUNTRY_NAME=
+# export Y_STATE_OR_PROVINCE_NAME=
+# export Y_LOCALITY_NAME=
+# export Y_ORGANIZATION_NAME=
+# export Y_ORGANIZATIONAL_UNIT_NAME=
+# export Y_EMAIL_ADDRESS=
+
+## test client
+# export Y_TEST_CLIENT_CREATE=no
+# export Y_TEST_CLIENT_PREFIX=tux1
+# export Y_TEST_CLIENT_CN=pc1.test.lan
+# export Y_TEST_CLIENT_PASSWORD=1234
+# export Y_TEST_CLIENT_REVO=yes
+# export Y_TEST_CLIENT_DAYS=31
+# export Y_TEST_CLIENT_SAN=DNS.1:pc1.my.net,IP.1:192.168.1.10
